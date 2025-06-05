6942fb077900f33894173dfa9ca1e8ad4e620ab1
nolan25 <favandnolan@gmail.com>
2025-06-05 13:26:11 +0200

    add low pages

--- config/bundles.php
+++ config/bundles.php
@@ -10,7 +10,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
---
+++ src/Controller/PageController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Job;
+use App\Repository\JobRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PageController extends AbstractController
+{
+   #[Route('/', name: 'home')]
+public function home(): Response
+{
+    return $this->render('page/home.html.twig');
+}
+#[Route('/about', name: 'about')]
+public function about(): Response
+{
+    return $this->render('page/about.html.twig');
+}
+#[Route('/jobs', name: 'job_list')]
+public function jobList(JobRepository $jobRepository): Response
+{
+    $jobs = $jobRepository->findAll();
+    return $this->render('page/job_list.html.twig', [
+        'jobs' => $jobs,
+    ]);
+}
+
+#[Route('/jobs/{id}', name: 'job_detail')]
+public function jobDetail(Job $job): Response
+{
+    return $this->render('page/job_detail.html.twig', [
+        'job' => $job,
+    ]);
+}
+}
